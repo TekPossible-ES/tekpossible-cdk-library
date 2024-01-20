eba13903746c6083f9270b193107ef0dde3666ec
Griffin Kiesecker <gkiesecker@Griffins-MacBook-Air.local>
2024-01-20 08:55:42 -0700

    Added a config folder so that I can parse that

---
+++ test/resume_aws_dev_ops.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ResumeAwsDevOps from '../lib/resume_aws_dev_ops-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/resume_aws_dev_ops-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ResumeAwsDevOps.ResumeAwsDevOpsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
