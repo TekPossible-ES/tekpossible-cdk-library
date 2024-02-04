42aff88d0cf037343ed95d512e84676d345f2b70
grifkies <grifkies@protonmail.com>
2024-02-04 14:55:12 -0700

    Added working on stuff

--- assets/devopsIaC/bootstrap.sh
+++ assets/devopsIaC/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo chown -R ec2-user /home/ec2-user/cdk
-cd /home/ec2-user/cdk
-npx cdk bootstrap
+yum install -y nodejs ruby
+cd /home/ec2-user/cdk/
+npm install sarif-junit@latest typescript cdk aws-cdk aws-cdk-lib
