78d143b607f70844021ac235f2acefab805b05d0
grifkies <grifkies@protonmail.com>
2024-02-04 12:20:54 -0700

    wow I really didnt install the cdk when I am linting the cdk code

--- assets/devopsIaC/configure.sh
+++ assets/devopsIaC/configure.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# THIS IS HERE FOR DOCUMENTATION - WILL BE RUN BEFORE CODEDEPLOY STEP IS RAN
 cd /home/ec2-user
 sudo dnf install ruby -y
 wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
-sudo dnf install -y ansible-core node
-sudo npm install -g typescript aws-cdk
+sudo dnf install -y ansible-core nodejs
+sudo npm install -g typescript aws-cdk cdk
