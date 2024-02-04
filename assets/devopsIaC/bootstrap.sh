3b9acdfe41bf8e880bca133812d5df9607e8c5ad
grifkies <grifkies@protonmail.com>
2024-02-04 15:17:37 -0700

    Working on the cdk issues I am having

--- assets/devopsIaC/bootstrap.sh
+++ assets/devopsIaC/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-yum install -y nodejs ruby
+sudo yum install -y nodejs ruby
+sudo chown -R ec2-user /home/ec2-user/cdk
 cd /home/ec2-user/cdk/
-npm install sarif-junit@latest typescript cdk aws-cdk aws-cdk-lib
+npm install sarif-junit@latest typescript cdk aws-cdk aws-cdk-lib util fs
--- assets/devopsIaC/deploy.sh
+++ assets/devopsIaC/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npx cdk deploy --all --no-notices --require-approval never
+cdk deploy --all --no-notices --require-approval never
