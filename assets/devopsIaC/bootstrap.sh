2009c42b0c5404d5e4c2fed401ad97bf3810a142
grifkies <grifkies@protonmail.com>
2024-02-04 16:50:47 -0700

    I believe

--- assets/devopsIaC/bootstrap.sh
+++ assets/devopsIaC/bootstrap.sh
@@ -2,4 +2,7 @@
 sudo chown -R ec2-user /home/ec2-user/cdk
 cd /home/ec2-user/cdk/
 npm install 
+npm update
+sudo npm install -g typescript ts-node aws-cdk
+npm install --save-dev @types/node
 npx cdk bootstrap
