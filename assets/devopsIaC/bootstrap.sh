b413ae00760f5184dd2ce211165d276ed1dc26be
grifkies <grifkies@protonmail.com>
2024-02-04 17:23:54 -0700

    Okay this should work

--- assets/devopsIaC/bootstrap.sh
+++ assets/devopsIaC/bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 sudo chown -R ec2-user /home/ec2-user/cdk
 cd /home/ec2-user/cdk/
+rm -rf ./node_modules
+rm -rf ./package*.json
 npm install 
 npm update
 sudo npm install -g typescript ts-node aws-cdk
+npm install aws-cdk-lib 
 npm install --save-dev @types/node
 npx cdk bootstrap
